13cdd0bed89471c28d5aadb7e087e9c458615e5e
folkert van heusden <mail@vanheusden.com>
2022-10-04 18:55:25 +0200

    no strace

--- app/run.sh
+++ app/run.sh
@@ -2,6 +2,6 @@
 
 cd /home/folkert/Projects/esp32chesstest/app
 
-strace -s 1500 -f /usr/games/polyglot -noini -log true -ec ./wrapper.sh 2> bla.txt
+/usr/games/polyglot -noini -log true -ec ./wrapper.sh 2> bla.txt
 
 >&2 echo exit polyglot
