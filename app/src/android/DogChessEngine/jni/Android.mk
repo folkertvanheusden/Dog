a5881d2af61490d339132e6568ea4a9640f2cd49
folkert van heusden <mail@vanheusden.com>
2022-10-19 22:53:32 +0200

    android changes

--- app/src/android/DogChessEngine/jni/Android.mk
+++ app/src/android/DogChessEngine/jni/Android.mk
@@ -8,6 +8,6 @@ LOCAL_SRC_FILES := ../../../eval.cpp ../../../eval_par.cpp ../../../main.cpp ../
 LOCAL_CPPFLAGS := -std=gnu++17 -Wall -fPIC -DVERSION=\"0.8\" -DNAME_EXTRA=\"\" -fexceptions -I../../../include/ -Wno-c++11-narrowing # whatever g++ flags you like
 LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -fPIC -pie   # whatever ld flags you like
 
-LOCAL_LDFLAGS := -shared
+#LOCAL_LDFLAGS := -shared
 
 include $(BUILD_SHARED_LIBRARY)
--- app/src/android/DogChessEngine/jni/Application.mk
+++ app/src/android/DogChessEngine/jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI := armeabi-v7a
+APP_ABI := all
 NDK_TOOLCHAIN_VERSION := 4.9
 APP_STL := c++_static
 APP_CPPFLAGS += -std=c++17
