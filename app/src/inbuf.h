3d36436767866c2b5a491ede4f98805c02fbedd6
Folkert van Heusden <mail@vanheusden.com>
2024-12-13 16:40:25 +0100

    restructuring

---
+++ app/src/inbuf.h
@@ -0,0 +1,83 @@
+#include <cstdio>
+#include <streambuf>
+
+
+// http://www.josuttis.com/libbook/io/inbuf1.hpp.html
+class inbuf : public std::streambuf {
+protected:
+	/* data buffer:
+	 * - at most, four characters in putback area plus
+	 * - at most, six characters in ordinary read buffer
+	 */
+	static const int bufferSize = 10;    // size of the data buffer
+	char buffer[bufferSize];             // data buffer
+	bool local_echo { false };
+
+public:
+	/* constructor
+	 * - initialize empty data buffer
+	 * - no putback area
+	 * => force underflow()
+	 */
+	inbuf() {
+		setg (buffer+4,     // beginning of putback area
+				buffer+4,     // read position
+				buffer+4);    // end position
+	}
+
+	void set_local_echo(const bool state) {
+		local_echo = state;
+	}
+
+protected:
+	// insert new characters into the buffer
+	virtual int_type underflow () {
+		// is read position before end of buffer?
+		if (gptr() < egptr())
+			return traits_type::to_int_type(*gptr());
+
+		/* process size of putback area
+		 * - use number of characters read
+		 * - but at most four
+		 */
+		int numPutback;
+		numPutback = gptr() - eback();
+		if (numPutback > 4) {
+			numPutback = 4;
+		}
+
+		/* copy up to four characters previously read into
+		 * the putback buffer (area of first four characters)
+		 */
+		std::memmove (buffer+(4-numPutback), gptr()-numPutback,
+				numPutback);
+
+		// read new characters
+		int c = 0;
+
+		for(;;) {
+			c = fgetc(stdin);
+			if (c >= 0) {
+				if (local_echo)
+					printf("%c", c);
+				break;
+			}
+
+#if !defined(linux) && !defined(_WIN32) && !defined(__ANDROID__)
+			vTaskDelay(1);
+#endif
+		}
+
+		buffer[4] = c;
+
+		int num = 1;
+
+		// reset buffer pointers
+		setg (buffer+(4-numPutback),   // beginning of putback area
+				buffer+4,                // read position
+				buffer+4+num);           // end of buffer
+
+		// return next character
+		return traits_type::to_int_type(*gptr());
+	}
+};
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -51,6 +51,7 @@
 
 #include "eval.h"
 #include "eval_par.h"
+#include "inbuf.h"
 #include "max-ascii.h"
 #include "psq.h"
 #if defined(linux) || defined(_WIN32)
@@ -245,86 +246,6 @@ auto position_handler = [](const libchess::UCIPositionParameters & position_para
 		positiont1.make_move(*libchess::Move::from(move_str));
 };
 
-
-// http://www.josuttis.com/libbook/io/inbuf1.hpp.html
-class inbuf : public std::streambuf {
-protected:
-	/* data buffer:
-	 * - at most, four characters in putback area plus
-	 * - at most, six characters in ordinary read buffer
-	 */
-	static const int bufferSize = 10;    // size of the data buffer
-	char buffer[bufferSize];             // data buffer
-	bool local_echo { false };
-
-public:
-	/* constructor
-	 * - initialize empty data buffer
-	 * - no putback area
-	 * => force underflow()
-	 */
-	inbuf() {
-		setg (buffer+4,     // beginning of putback area
-				buffer+4,     // read position
-				buffer+4);    // end position
-	}
-
-	void set_local_echo(const bool state) {
-		local_echo = state;
-	}
-
-protected:
-	// insert new characters into the buffer
-	virtual int_type underflow () {
-		// is read position before end of buffer?
-		if (gptr() < egptr())
-			return traits_type::to_int_type(*gptr());
-
-		/* process size of putback area
-		 * - use number of characters read
-		 * - but at most four
-		 */
-		int numPutback;
-		numPutback = gptr() - eback();
-		if (numPutback > 4) {
-			numPutback = 4;
-		}
-
-		/* copy up to four characters previously read into
-		 * the putback buffer (area of first four characters)
-		 */
-		std::memmove (buffer+(4-numPutback), gptr()-numPutback,
-				numPutback);
-
-		// read new characters
-		int c = 0;
-
-		for(;;) {
-			c = fgetc(stdin);
-			if (c >= 0) {
-				if (local_echo)
-					printf("%c", c);
-				break;
-			}
-
-#if !defined(linux) && !defined(_WIN32) && !defined(__ANDROID__)
-			vTaskDelay(1);
-#endif
-		}
-
-		buffer[4] = c;
-
-		int num = 1;
-
-		// reset buffer pointers
-		setg (buffer+(4-numPutback),   // beginning of putback area
-				buffer+4,                // read position
-				buffer+4+num);           // end of buffer
-
-		// return next character
-		return traits_type::to_int_type(*gptr());
-	}
-};
 
 inbuf i;
 std::istream is(&i);
