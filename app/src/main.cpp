386904af5d0af91cca169742921a62b6dbedda34
Folkert van Heusden <mail@vanheusden.com>
2024-12-19 23:44:18 +0100

    did not reduce depth

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -715,7 +715,7 @@ int search(libchess::Position & pos, int8_t depth, int16_t alpha, int16_t beta, 
 		pos.make_move(move);
 		int score = -10000;
 		if (first_move) {
-			score = -search(pos, depth + extension, -beta, -alpha, null_move_depth, max_depth, &new_move, sp, thread_nr);
+			score = -search(pos, depth - 1 + extension, -beta, -alpha, null_move_depth, max_depth, &new_move, sp, thread_nr);
 			first_move = false;
 		}
 		else {
