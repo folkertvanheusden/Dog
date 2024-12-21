7ab90cdc2ef0e7c55406f3e917108e94c96738f0
Folkert van Heusden <mail@vanheusden.com>
2024-12-21 23:25:14 +0100

    reset ESP32 when starting wrapper.sh

--- app/wrapper.sh
+++ app/wrapper.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-#/usr/bin/socat -v /dev/ttyACM0,b115200,echo=0,raw  PTY,link=myserial,raw,echo=0
+./toggle.py /dev/ttyACM1
 
-/usr/bin/socat -s -v STDIO,echo=0,raw /dev/ttyUSB0,raw,echo=0,b115200 2> err.txt
+/usr/bin/socat -s -v STDIO,echo=0,raw /dev/ttyACM1,raw,echo=0,b115200 2> err.txt
 
 >&2 echo exit socat
