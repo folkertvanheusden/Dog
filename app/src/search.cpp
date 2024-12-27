a3a64a2ca7e823fbf9320c981be7f0e284356e8f
Folkert van Heusden <mail@vanheusden.com>
2024-12-28 00:31:10 +0100

    layout

--- app/src/search.cpp
+++ app/src/search.cpp
@@ -375,9 +375,9 @@ int search(libchess::Position & pos, int8_t depth, int16_t alpha, int16_t beta, 
 			continue;
 
                 bool is_lmr = false;
+                int  score  = -10000;
 
                 pos.make_move(move);
-                int score = -10000;
                 if (n_played == 0)
                         score = -search(pos, depth - 1, -beta, -alpha, null_move_depth, max_depth, &new_move, sp, thread_nr);
                 else {
@@ -407,8 +407,7 @@ int search(libchess::Position & pos, int8_t depth, int16_t alpha, int16_t beta, 
 
 		if (score > best_score) {
 			best_score = score;
-
-			*m = move;
+			*m         = move;
 
 			if (score > alpha) {
 				if (score >= beta) {
