812c9426c11dd6fc3e43a67e5ed2eaccc936650e
Folkert van Heusden <mail@vanheusden.com>
2024-12-28 11:21:46 +0100

    missing changes (2)

---
+++ app/src/stats.h
@@ -0,0 +1,45 @@
+#include <cstdint>
+
+class chess_stats
+{
+public:
+	struct {
+		uint32_t  nodes;
+		uint32_t  qnodes;
+
+		uint32_t  n_draws;
+
+		uint32_t  alpha_distance;
+		uint32_t  beta_distance;
+		int       n_alpha_distances;
+		int       n_beta_distances;
+
+		uint32_t  tt_query;
+		uint32_t  tt_hit;
+		uint32_t  tt_store;
+		uint32_t  tt_invalid;
+
+		uint32_t  n_null_move;
+		uint32_t  n_null_move_hit;
+
+		uint32_t  n_lmr;
+		uint32_t  n_lmr_hit;
+
+		uint32_t  n_static_eval;
+		uint32_t  n_static_eval_hit;
+
+		uint64_t  n_moves_cutoff;
+		uint64_t  nmc_nodes;
+		uint64_t  n_qmoves_cutoff;
+		uint64_t  nmc_qnodes;
+
+		uint64_t  syzygy_queries;
+		uint64_t  syzygy_query_hits;
+	} data;
+
+	chess_stats();
+	virtual ~chess_stats();
+
+	void reset();
+	void add(const chess_stats *const in);
+};
