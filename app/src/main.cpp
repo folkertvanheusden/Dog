9a82bd16482fa168aef2d3e329f3bcb20836c35c
Folkert van Heusden <mail@vanheusden.com>
2024-12-28 21:54:02 +0100

    bench: 3607278

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -881,8 +881,8 @@ void run_bench()
 	memset(sp1.history, 0x00, history_malloc_size);
 
 	chess_stats    cs;
-	libchess::Move best_move  { 0     };
-	int            best_score { 0     };
+	libchess::Move best_move  { 0 };
+	int            best_score { 0 };
 	sp1.is_t2 = false;
 
 	uint64_t start_ts = esp_timer_get_time();
