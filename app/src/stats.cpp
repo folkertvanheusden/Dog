9312fb245278179e2deb54a8e028cc79f09a04b7
Folkert van Heusden <mail@vanheusden.com>
2024-12-28 11:21:46 +0100

    missing changes (2)

---
+++ app/src/stats.cpp
@@ -0,0 +1,42 @@
+#include <cstring>
+
+#include "stats.h"
+
+
+chess_stats::chess_stats()
+{
+}
+
+chess_stats::~chess_stats()
+{
+}
+
+void chess_stats::reset()
+{
+	memset(&data, 0x00, sizeof data);
+}
+
+void chess_stats::add(const chess_stats *const source)
+{
+	this->data.nodes    += source->data.nodes;
+	this->data.qnodes   += source->data.qnodes;
+	this->data.n_draws  += source->data.n_draws;
+
+        this->data.tt_query += source->data.tt_query;
+        this->data.tt_hit   += source->data.tt_hit;
+        this->data.tt_store += source->data.tt_store;
+
+	this->data.n_null_move     += source->data.n_null_move;
+	this->data.n_null_move_hit += source->data.n_null_move_hit;
+
+	this->data.n_lmr     += source->data.n_lmr;
+	this->data.n_lmr_hit += source->data.n_lmr_hit;
+
+	this->data.n_static_eval     += source->data.n_static_eval;
+	this->data.n_static_eval_hit += source->data.n_static_eval_hit;
+
+	this->data.n_moves_cutoff  += source->data.n_moves_cutoff;
+	this->data.nmc_nodes       += source->data.nmc_nodes;
+	this->data.n_qmoves_cutoff += source->data.n_qmoves_cutoff;
+	this->data.nmc_qnodes      += source->data.nmc_qnodes;
+}
