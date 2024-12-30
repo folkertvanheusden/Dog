efedb39f5f1e774edfb4d88091dc106fa83874f8
Folkert van Heusden <mail@vanheusden.com>
2024-12-30 22:50:39 +0100

    LMR: yukari reduction

--- app/src/search.cpp
+++ app/src/search.cpp
@@ -394,10 +394,17 @@ int search(libchess::Position & pos, int8_t depth, int16_t alpha, int16_t beta, 
                                 is_lmr = true;
 				sp->cs->data.n_lmr++;
 
-				if (n_played >= lmr_start + 2)
+				if (alpha == beta -1) {
+					constexpr double lmr_mul  = 0.5;
+					constexpr double lmr_base = 1.0;
+					int reduction = log(depth) * log(n_played + 1) * lmr_mul + lmr_base;
+					new_depth = std::max(depth - reduction, 0);
+				}
+				else if (n_played >= lmr_start + 2)
 					new_depth = (depth - 1) * 2 / 3;
-				else
+				else {
 					new_depth = depth - 2;
+				}
 			}
 
                         score = -search(pos, new_depth, -alpha - 1, -alpha, null_move_depth, max_depth, &new_move, sp, thread_nr);
