ab8a663715d9e70d2262ff87e8cb82bf62430f96
Folkert van Heusden <mail@vanheusden.com>
2025-01-13 23:02:31 +0100

    do not calloc but malloc so that invalid reads are caught more easily
    bench: 9757471

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -365,7 +365,7 @@ void allocate_threads(const int n)
 	delete_threads();
 
 	for(int i=0; i<n; i++) {
-		sp.push_back(new search_pars_t({ default_parameters, reinterpret_cast<int16_t *>(calloc(1, history_malloc_size)), new end_t }));
+		sp.push_back(new search_pars_t({ default_parameters, reinterpret_cast<int16_t *>(malloc(history_malloc_size)), new end_t }));
 		sp.at(i)->thread_handle = new std::thread(searcher, i);
 	}
 #if defined(ESP32)
