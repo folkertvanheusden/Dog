bc979a0c70687cbd833f859b6219f093e7d9566e
Folkert van Heusden <mail@vanheusden.com>
2025-02-03 21:06:45 +0100

    condition variables on shared memory are not easily kept usable https://stackoverflow.com/questions/66494029/shared-pthread-cond-broadcast-stuck-in-futex-wait
    bench: 6152856

--- app/src/state_exporter.cpp
+++ app/src/state_exporter.cpp
@@ -1,7 +1,9 @@
 #include <cstring>
 #include <errno.h>
+#include <fcntl.h>
 #include <unistd.h>
-#include <sys/shm.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 #include "state_exporter.h"
 
@@ -9,20 +11,43 @@
 state_exporter::state_exporter(const int hz):
 	hz(hz)
 {
-	unsigned sid = shmget('Dog0', sizeof(_export_structure_), IPC_CREAT | 0600);
-	pdata        = reinterpret_cast<_export_structure_ *>(shmat(sid, nullptr, 0));
-	if (pdata != reinterpret_cast<void *>(-1)) {
-		pthread_mutexattr_t mutex_attributes;
+	bool new_segment = false;
+
+	// try to open
+	fd = shm_open("/Dog", O_RDWR, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP);
+	if (fd == -1) {
+		// did not exist
+		if (errno != ENOENT) {
+			fprintf(stderr, "Failed to access shared memory: %s\n", strerror(errno));
+			return;
+		}
+
+		// create
+		fd = shm_open("/Dog", O_CREAT|O_EXCL|O_RDWR, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP);
+		if (fd == -1) {
+			fprintf(stderr, "Failed to create shared memory: %s\n", strerror(errno));
+			return;
+		}
+
+		ftruncate(fd, sizeof(_export_structure_));
+
+		new_segment = true;
+	}
+
+	pdata = reinterpret_cast<_export_structure_ *>(mmap(nullptr, sizeof(_export_structure_), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0));
+	if (pdata == reinterpret_cast<void *>(-1))
+		pdata = nullptr;
+	else if (new_segment && sysconf(_SC_THREAD_PROCESS_SHARED) >= 0) {
+		pthread_mutexattr_t mutex_attributes { };
 		pthread_mutexattr_init(&mutex_attributes);
 		pthread_mutexattr_setpshared(&mutex_attributes, PTHREAD_PROCESS_SHARED);
 		pthread_mutex_init(&pdata->mutex, &mutex_attributes);
+	}
 
+	if (pdata)
 		th = new std::thread(&state_exporter::handler, this);
-	}
-	else {
+	else
 		fprintf(stderr, "SHM unavailable: %s\n", strerror(errno));
-		pdata = nullptr;
-	}
 }
 
 state_exporter::~state_exporter()
@@ -34,21 +59,26 @@ state_exporter::~state_exporter()
 		delete th;
 	}
 
-	shmdt(pdata);
+	if (pdata)
+		munmap(pdata, 0);
+	if (fd != -1)
+		close(fd);
 }
 
 void state_exporter::set(search_pars_t *const sp)
 {
 	std::unique_lock<std::mutex> lck(lock);
 	this->sp = sp;
-	pdata->revision = 0;
+	if (pdata)
+		pdata->revision = 0;
 }
 
 void state_exporter::clear()
 {
 	std::unique_lock<std::mutex> lck(lock);
 	sp = nullptr;
-	pdata->revision = 0;
+	if (pdata)
+		pdata->revision = 0;
 }
 
 void state_exporter::handler()
@@ -60,11 +90,16 @@ void state_exporter::handler()
 		if (!sp)
 			continue;
 
-		if (pthread_mutex_trylock(&pdata->mutex) == 0) {
+		int rc = pthread_mutex_trylock(&pdata->mutex);
+		if (rc == 0) {
+			static int cnt = 0;
 			pdata->counters = sp->cs.data;
 			pdata->cur_move = sp->cur_move;
 			pdata->revision++;
 			pthread_mutex_unlock(&pdata->mutex);
+		}
+		else if (rc == EOWNERDEAD) {
+			pthread_mutex_consistent(&pdata->mutex);
 		}
 	}
 }
