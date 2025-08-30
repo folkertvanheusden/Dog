03f806ee8342c18f7c162bee1900bc15c090e62e
Folkert van Heusden <mail@vanheusden.com>
2025-08-30 19:12:01 +0200

    corrected port

--- app/src/gen-train-data.py
+++ app/src/gen-train-data.py
@@ -18,7 +18,7 @@ import time
 #logging.basicConfig(level=logging.DEBUG)
 
 host = 'dog.vanheusden.com'
-port = 31251
+port = 31250
 proc = None
 node_count = 10000
 nth = os.sysconf('SC_NPROCESSORS_ONLN')
