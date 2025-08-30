3f1897ba6bd01260835f744164b1cd3335a41241
Folkert van Heusden <mail@vanheusden.com>
2025-08-30 20:08:45 +0200

    only use VALGRIND/DRD when in DEBUG mode

--- app/src/tt.cpp
+++ app/src/tt.cpp
@@ -1,7 +1,7 @@
 #include <cinttypes>
 #include <cstdlib>
 #include <cstring>
-#if !defined(_WIN32) && !defined(ESP32)
+#if (!defined(_WIN32) && !defined(ESP32) && !defined(__ANDROID__) && !defined(__APPLE__)) && DEBUG==1
 #include <valgrind/helgrind.h>
 #endif
 
