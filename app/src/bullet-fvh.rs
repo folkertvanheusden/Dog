8872e9312726201dab4e5d0d4d7e416ae49313e7
Folkert van Heusden <mail@vanheusden.com>
2025-09-10 15:12:02 +0200

    +15 relative to Dog-v3.4-003-native

--- app/src/bullet-fvh.rs
+++ app/src/bullet-fvh.rs
@@ -1,4 +1,3 @@
-
 /*
 This is about as simple as you can get with a network, the arch is
     (768 -> HIDDEN_SIZE)x2 -> 1
@@ -7,18 +6,20 @@ There's potentially a lot of elo available by adjusting the wdl
 and lr schedulers, depending on your dataset.
 */
 use bullet_lib::{
-    nn::{optimiser, Activation},
-    trainer::{
-        default::{
-            formats::sfbinpack::{
-                chess::{piecetype::PieceType, r#move::MoveType},
-                TrainingDataEntry,
-            },
-            inputs, loader, outputs, Loss, TrainerBuilder,
+    game::{
+        formats::sfbinpack::{
+            TrainingDataEntry,
+            chess::{r#move::MoveType, piecetype::PieceType},
         },
-        schedule::{lr, wdl, TrainingSchedule, TrainingSteps},
+        inputs,
+    },
+    nn::optimiser,
+    trainer::{
+        save::SavedFormat,
+        schedule::{TrainingSchedule, TrainingSteps, lr, wdl},
         settings::LocalSettings,
     },
+    value::{ValueTrainerBuilder, loader},
 };
 
 const HIDDEN_SIZE: usize = 128;
@@ -27,42 +28,59 @@ const QA: i16 = 255;
 const QB: i16 = 64;
 
 fn main() {
-    let mut trainer = TrainerBuilder::default()
-        .quantisations(&[QA, QB])
+    let mut trainer = ValueTrainerBuilder::default()
+        // makes `ntm_inputs` available below
+        .dual_perspective()
+        // standard optimiser used in NNUE
+        // the default AdamW params include clipping to range [-1.98, 1.98]
         .optimiser(optimiser::AdamW)
-        .loss_fn(Loss::SigmoidMSE)
-        .input(inputs::Chess768)
-        .output_buckets(outputs::Single)
-        .feature_transformer(HIDDEN_SIZE)
-        .activate(Activation::SCReLU)
-        .add_layer(1)
-        .build();
+        // basic piece-square chessboard inputs
+        .inputs(inputs::Chess768)
+        // chosen such that inference may be efficiently implemented in-engine
+        .save_format(&[
+            SavedFormat::id("l0w").quantise::<i16>(255),
+            SavedFormat::id("l0b").quantise::<i16>(255),
+            SavedFormat::id("l1w").quantise::<i16>(64),
+            SavedFormat::id("l1b").quantise::<i16>(255 * 64),
+        ])
+        // map output into ranges [0, 1] to fit against our labels which
+        // are in the same range
+        // `target` == wdl * game_result + (1 - wdl) * sigmoid(search score in centipawns / SCALE)
+        // where `wdl` is determined by `wdl_scheduler`
+        .loss_fn(|output, target| output.sigmoid().squared_error(target))
+        // the basic `(768 -> N)x2 -> 1` inference
+        .build(|builder, stm_inputs, ntm_inputs| {
+            // weights
+            let l0 = builder.new_affine("l0", 768, HIDDEN_SIZE);
+            let l1 = builder.new_affine("l1", 2 * HIDDEN_SIZE, 1);
 
-    let initial_lr = 0.001;
-    let final_lr = 0.001 * 0.3f32.powi(5);
-    let superbatches = 320;
+            // inference
+            let stm_hidden = l0.forward(stm_inputs).screlu();
+            let ntm_hidden = l0.forward(ntm_inputs).screlu();
+            let hidden_layer = stm_hidden.concat(ntm_hidden);
+            l1.forward(hidden_layer)
+        });
 
     let schedule = TrainingSchedule {
-        net_id: "1_simple".to_string(),
-        eval_scale: 400.0,
+        net_id: "simple".to_string(),
+        eval_scale: SCALE as f32,
         steps: TrainingSteps {
             batch_size: 16_384,
             batches_per_superbatch: 6104,
             start_superbatch: 1,
-            end_superbatch: superbatches,
+            end_superbatch: 62,
         },
-        wdl_scheduler: wdl::ConstantWDL { value: 1.0 },
-        lr_scheduler: lr::CosineDecayLR { initial_lr, final_lr, final_superbatch: superbatches },
+        wdl_scheduler: wdl::ConstantWDL { value: 0.75 },
+        lr_scheduler: lr::StepLR { start: 0.001, gamma: 0.1, step: 18 },
         save_rate: 10,
     };
 
-    trainer.set_optimiser_params(optimiser::AdamWParams::default());
+    let settings = LocalSettings { threads: 32, test_set: None, output_directory: "checkpoints", batch_queue_size: 64 };
 
-    let settings = LocalSettings { threads: 16, test_set: None, output_directory: "checkpoints", batch_queue_size: 64 };
+    // loading directly from a `BulletFormat` file
+    let data_loader = loader::DirectSequentialDataLoader::new(&["/home/folkert/combined.dat"]);
 
-    let data_loader = loader::DirectSequentialDataLoader::new(&["/home/folkert/test.bin"]);
     trainer.run(&schedule, &settings, &data_loader);
-
 }
 
 /*
