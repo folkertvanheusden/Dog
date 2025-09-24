089f8ce49a5d357916aca5d6b381d57c8088ce0b
Folkert van Heusden <mail@vanheusden.com>
2025-09-24 21:21:18 +0200

    emit latencies + counts

--- app/src/linux-windows/build/tijd-test.py
+++ app/src/linux-windows/build/tijd-test.py
@@ -10,6 +10,7 @@ n_games = 0
 players = dict()
 m_counts = dict()
 nm_counts = dict()
+latencies = dict()
 
 time_control = None
 
@@ -25,11 +26,13 @@ while True:
         players[cw] = []
         nm_counts[cw] = 0
         m_counts[cw] = 0
+        latencies[cw] = dict()
     cb = game.headers['Black']
     if not cb in players:
         players[cb] = []
         nm_counts[cb] = 0
         m_counts[cb] = 0
+        latencies[cb] = dict()
     p = [None, None]
     p[chess.WHITE] = cw
     p[chess.BLACK] = cb
@@ -42,32 +45,39 @@ while True:
         players[p[b.turn]][b.fullmove_number][1] += 1
         b.push(move.move)
         m_counts[p[b.turn]] += 1
+        latency = parts[3][8:].rstrip('s,')
+        if not latency in latencies[p[b.turn]]:
+            latencies[p[b.turn]][latency] = 0
+        latencies[p[b.turn]][latency] += 1
     nm_counts[cw] += 1
     nm_counts[cb] += 1
     n_games += 1
 
-print('/usr/bin/gnuplot << EOF > output.png')
-print('set term png size 1920,1080')
-print('set autoscale')
-print(f'set title "average time used per move, over {n_games} games, {time_control} (tc)"')
-print('set boxwidth 0.4')
-print('set grid')
-print('set xlabel "move nr"')
-print('set ylabel "avg time (seconds)"')
+fhs = open('tijd-test1.sh', 'w')
+fhs.write('/usr/bin/gnuplot << EOF > output.png\n')
+fhs.write('set term png size 1920,1080\n')
+fhs.write('set autoscale\n')
+fhs.write(f'set title "average time used per move, over {n_games} games, {time_control} (tc)"\n')
+fhs.write('set boxwidth 0.4\n')
+fhs.write('set grid\n')
+fhs.write('set xlabel "move nr"\n')
+fhs.write('set ylabel "avg time (seconds)"\n')
 
 first = True
 for p in players:
+    print(latencies[p])
     fname = p + '.dat'
-    fh = open(fname, 'w')
+    fhd = open(fname, 'w')
     nr = 1
     for pair in players[p]:
-        fh.write(f'{nr} {pair[0] / pair[1] if pair[1] else 0}\n')
+        fhd.write(f'{nr} {pair[0] / pair[1] if pair[1] else 0}\n')
         nr += 1
-    fh.close()
+    fhd.close()
     if first:
         first = False
-        print(f'plot "{fname}" using 1:2 axes x1y1 with lines title "{p} ({nm_counts[p]} games)" \\')
+        fhs.write(f'plot "{fname}" using 1:2 axes x1y1 with lines title "{p} ({nm_counts[p]} games)" \\\n')
     else:
-        print(f',    "{fname}" using 1:2 axes x1y1 with lines title "{p} ({nm_counts[p]} games)" \\')
-print('')
-print('EOF')
+        fhs.write(f',    "{fname}" using 1:2 axes x1y1 with lines title "{p} ({nm_counts[p]} games)" \\\n')
+fhs.write('')
+fhs.write('EOF\n')
+fhs.close()
