041a4e2e57e28d804cf9b75b79d6907aef721fbf
Folkert van Heusden <mail@vanheusden.com>
2025-10-25 21:33:30 +0200

    use sygyzy

--- app/run.sh
+++ app/run.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-cd /home/folkert/Projects/esp32chesstest/app
+cd /home/folkert/Projects/Dog/app
 
 #/usr/games/polyglot -noini -log true -ec ./wrapper.sh 2> bla.txt
-/usr/games/polyglot -noini -log true -ec ./wrapper-local.sh 2> bla.txt
+/usr/games/polyglot -noini -log true -ec ./wrapper-local-syzygy.sh 2> bla.txt
 
 >&2 echo exit polyglot
